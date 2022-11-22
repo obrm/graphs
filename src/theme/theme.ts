3f534067d3583a1fc189d6c2af0cd0deeab2fa66
Ori Baram <obrm770@gmail.com>
2022-11-22 16:31:32 +0200

    refactoring

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,11 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     mode: 'dark',
-    primary: {
-      main: '#251E3F',
-    },    
   },
 });
 
 export default theme;
-
